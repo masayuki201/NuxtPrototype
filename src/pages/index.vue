dbd4340243f80a302b0b07a8c342c59ff4baa7dd
masayuki201 <masayuki201_201@yahoo.co.jp>
2023-03-14 08:21:56 +0900

    add:create pages for index

---
+++ src/pages/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const { fetchArticles, articles } = useArticles();
+fetchArticles();
+</script>
+
+<template>
+  <div>
+    <p>新着記事！！</p>
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        <NuxtLink :to="{path: '/details', query: { id:article.id }}">
+          {{article.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+    <Advertisement />
+  </div>
+</template>
+
+<style scoped>
+header {
+  border-bottom-style: solid;
+  padding: 1rem;
+}
+
+footer {
+  margin-top: 2rem;
+  background-color: #8080ee;
+  padding: 1rem;
+}
+
+.container {
+  margin: 2rem;
+}
+</style>
