93e8260b49fd7a7d3a61ae33a78d4efea3c57c9b
masayuki201 <masayuki201_201@yahoo.co.jp>
2023-03-20 07:44:09 +0900

    add:Dynamic Routing

---
+++ src/pages/users/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const router = useRoute();
+
+console.log(router.params.id);
+</script>
+
+<template>
+  <p>ユーザID: {{ $route.params.id }}</p>
+</template>
+
+<style scoped>
+
+</style>
