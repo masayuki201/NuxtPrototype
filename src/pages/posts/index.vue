1ebed346a2c86d29135a6242d82416d85e4ddc07
masayuki201 <masayuki201_201@yahoo.co.jp>
2023-03-20 19:17:05 +0900

    add:useLazyAsyncData

--- src/pages/posts/index.vue
+++ src/pages/posts/index.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
-const {data: posts, error, refresh} = await useFetch(
-    'https://jsonplaceholder.typicode.com/posts/'
+//usefetchの場合
+// const {
+//   data: posts, error, refresh
+// } = await useFetch(
+//     'https://jsonplaceholder.typicode.com/posts/'
+// );
+
+//useLazyAsyncDataの場合
+const {
+  data: posts,
+  error,
+  pending,
+  refresh,
+} = await useLazyAsyncData('posts', () =>
+    $fetch('https://jsonplaceholder.typicode.com/posts/')
 );
+console.log(posts);
+console.log(useNuxtApp().payload.data);
+
+
 </script>
 
 <template>
@@ -9,8 +26,13 @@ const {data: posts, error, refresh} = await useFetch(
     <h1>Posts一覧</h1>
     <button @click="refresh()">再取得</button>
     <p v-if="error">{{ error }}</p>
+    <p v-if="pending">Loading...</p>
     <ul>
-      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+      <li v-for="post in posts" :key="post.id">
+        <NuxtLink :to="`/posts/${post.id}`">
+          {{ post.title }}
+        </NuxtLink>
+      </li>
     </ul>
   </div>
 </template>
